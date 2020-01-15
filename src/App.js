73b503cd38823fd1f385d27b22f9c6287925c7c5
Gerard Genovese <gmoneymaster24@yahoo.com>
2020-01-15 17:02:41 -0500

    added redux implemented userReducer userAction to header and app componenets

--- src/App.js
+++ src/App.js
@@ -1,44 +1,59 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { setCurrentUser } from './redux/user/user.actions';
+
+
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-import { Switch, Route } from 'react-router-dom';
+
 
 import './App.css';
 
 class App extends React.Component {
-  constructor(){
-    super();
+  // constructor(){
+  //   super();
 
-    this.state = {
-      currentUser: null
-    }
-  }
+  //   this.state = {
+  //     currentUser: null
+  //   }
+  // }
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       //check is there is a userAuth IE user is signed in
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         //subscribe/listen to userRef for changes & setState
-        userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          }/*,() => { console.log(this.state); }*/)
+        // userRef.onSnapshot(snapShot => {
+        //   this.setState({
+        //     currentUser: {
+        //       id: snapShot.id,
+        //       ...snapShot.data()
+        //     }
+        //   }/*,() => { console.log(this.state); }*/)
     
-        });
+        // });
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          })
+        })
       } else {
-        this.setState({ currentUser: null });
+        // this.setState({ currentUser: null });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -50,7 +65,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -60,6 +75,10 @@ class App extends React.Component {
       </div>
     );
   }
- 
 }
-export default App;
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
